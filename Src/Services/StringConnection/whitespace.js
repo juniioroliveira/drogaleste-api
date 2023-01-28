3b23b45daa400da0a65d937fb725d12c3703fe5b
Junior Oliveira <junioroliveira.la@hotmail.com>
2023-01-28 00:47:27 -0300

    first commit

---
+++ Src/Services/StringConnection/whitespace.js
@@ -0,0 +1,13 @@
+var config = {
+    user: 'drogaleste.junior',
+    password: 'Ti873562',
+    server: 'drogaleste.procfit.com.br', // You can use 'localhost\\instance' to connect to named instance
+    database: 'PBS_DROGALESTE_DADOS',
+    port: 1433,
+
+    options: {
+        encrypt: false // Use this if you're on Windows Azure
+    }
+  };
+
+  module.exports = config;
---
+++ Src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// App
+const app = express();
+
+// Load routes DrogalesteAPI
+const drogalesteRoutes = require('./Routes/drogaleste');
+app.use('/api/drogaleste', drogalesteRoutes);
+
+// Load routes WhitespaceAPI
+const whitespaceRoutes = require('./Routes/whitespace');
+app.use('/api/whitespace', whitespaceRoutes);
+
+module.exports = app;
