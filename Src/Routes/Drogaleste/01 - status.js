7b4cf0369fca4846d0339172ec8c3a22fee00adf
Junior Oliveira <junioroliveira.la@hotmail.com>
2023-02-22 16:06:57 -0300

    modified:   Src/Routes/Drogaleste/01 - status.js

--- Src/Routes/Drogaleste/01 - status.js
+++ Src/Routes/Drogaleste/01 - status.js
@@ -4,7 +4,7 @@ const reportLog = require('../../Services/Functions/reportLog');
 
 const package = require('../../../package.json');
 
-//STATUS API
+//STATUS API 
 router.get('/', (req, res, next) => {
 
   reportLog('Processo: Solicitação de status da api')
