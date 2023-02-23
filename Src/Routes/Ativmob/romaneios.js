9310c674f2dcf24e36468d5ac04cff5e7a195d3e
Junior Oliveira <junioroliveira.la@hotmail.com>
2023-02-23 15:28:45 -0300

    new file:   Src/Routes/Ativmob/romaneios.js 	modified:   swagger_output.json

---
+++ Src/Routes/Ativmob/romaneios.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router()
+const reportLog = require('../../Services/Functions/reportLog');
+
+const package = require('../../../package.json');
+
+//STATUS API 
+router.get('/', (req, res, next) => {
+
+  reportLog('Processo: Solicitação de status da api')
+    res.status(200).send({
+      title: 'Drogaleste-API',
+      version: package.version,
+      status: 'Operando'
+    });
+  }); 
+   
+module.exports = router;
+
