b7c478507c3907de1a9dc0a3476b1546b67bd557
Junior Oliveira <junioroliveira.la@hotmail.com>
2023-02-28 00:15:18 -0300

    modified:   Src/Services/Functions/drogalesteConnect.js

--- Src/Services/Functions/drogalesteConnect.js
+++ Src/Services/Functions/drogalesteConnect.js
@@ -17,60 +17,74 @@ async function execSQLDrogalesteQuery(script, res){
     sql.connect(strConn)
         .then(async conn => {
            response = await conn.request()
-                                .query(script)
-                                .then(async content => messageSucess = await content.recordset[0].res)
-                                .catch(async err =>  messageError = await err);
-                                if(response){
-                                    
-                                    if(messageError){
-                                        var error = {
-                                            code: 500,
-                                            message: 'Erro ao retornar dados',
-                                            ex: messageError.code
-                                        }
-        
-                                        reportLog(`Code:       ${error.code}`)
-                                        reportLog(`Message:    ${error.message}`)
-                                        reportLog(`Ex:         ${error.ex}`)        
-                                        console.log('');
-        
-                                        res.status(500).send(error);
-                                    }
-                                    
-                                    if(messageSucess){
-                                        
-                                        console.log(messageSucess);
-                                        
-                                        var error = {
-                                            code: 200,
-                                            message: 'Dados retornados com sucesso'
-                                        }
-        
-                                        reportLog(`Code:       ${error.code}`)
-                                        reportLog(`Message:    ${error.message}`) 
-                                        console.log('');
-    
-                                        console.log(messageSucess);
-                                        
-                                        res.status(200).send(JSON.parse(messageSucess))
-                                    }
+                .query(script)
+                .then(async content => messageSucess = await content.recordset[0].res)
+                .catch(async err =>  messageError = await err);
 
-                                }else{
-                                    var error = {
-                                        code: 404,
-                                        message: 'Nenhum dado encontrado',
-                                        ex: 'Sua requisição não retornou nenhuma dado'
-                                    }
-    
-                                    reportLog(`Code:       ${error.code}`)
-                                    reportLog(`Message:    ${error.message}`)
-                                    reportLog(`Ex:         ${error.ex}`)        
-                                    console.log('');
-    
-                                    res.status(404).send(error);
-                                }
-                            }
-                );
+            if(response){
+                
+                if(messageError){
+                    var error = {
+                        code: 500,
+                        message: 'Erro ao retornar dados',
+                        ex: messageError.code
+                    }
+
+                    reportLog(`Code:       ${error.code}`)
+                    reportLog(`Message:    ${error.message}`)
+                    reportLog(`Ex:         ${error.ex}`)        
+                    console.log('');
+
+                    res.status(500).send(error);
+                }
+                
+                if(messageSucess){
+                    
+                    console.log(messageSucess);
+                    
+                    var error = {
+                        code: 200,
+                        message: 'Dados retornados com sucesso'
+                    }
+
+                    reportLog(`Code:       ${error.code}`)
+                    reportLog(`Message:    ${error.message}`) 
+                    console.log('');
+
+                    console.log(messageSucess);
+                    
+                    res.status(200).send(JSON.parse(messageSucess))
+                }
+
+            }else{
+                var error = {
+                    code: 404,
+                    message: 'Nenhum dado encontrado',
+                    ex: 'Sua requisição não retornou nenhuma dado'
+                }
+
+                reportLog(`Code:       ${error.code}`)
+                reportLog(`Message:    ${error.message}`)
+                reportLog(`Ex:         ${error.ex}`)        
+                console.log('');
+
+                res.status(404).send(error);
+            }
+                }
+        ).catch(async error =>  {
+            var error = {
+                code: 500,
+                message: 'Erro ao retornar dados',
+                ex: messageError.code
+            }
+
+            reportLog(`Code:       ${error.code}`)
+            reportLog(`Message:    ${error.message}`)
+            reportLog(`Ex:         ${error.ex}`)        
+            console.log('');
+
+            res.status(500).send(error);
+        });;
 }
 
 module.exports = execSQLDrogalesteQuery;
