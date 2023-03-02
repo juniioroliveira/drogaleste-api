fcd6939c46470657c41068aed5b0e4822df3a09f
Junior Oliveira <junioroliveira.la@hotmail.com>
2023-03-02 18:21:04 -0300

    modified:   Src/Routes/Drogaleste/User.js

--- Src/Routes/Drogaleste/User.js
+++ Src/Routes/Drogaleste/User.js
@@ -60,78 +60,11 @@ router.post('/new', async (req, res, next) => {
             return;
           }
 
-          // var response = await execSQLDrogaleste(`EXEC API_NEW_USER_POST '${nome}', ${inscricaoFederal}, '${email}'`)
+          var response = await execSQLDrogaleste(`EXEC API_NEW_USER_POST '${nome}', ${inscricaoFederal}, '${email}'`)
 
-          let response = {status: 1};
 
           const {status} = response;
           if(status == 1){
-            // if(status == 1){
-
-              // const htmlUsuario = 
-              // `<div class="SlLx9 byzS1" style="margin-right: 20px; margin-left: 8px; padding: 0px 12px 12px; width: calc(100% - 53.5px);" tabindex="-1" aria-label="Mensagem de email">` +
-              // `<div>&nbsp;<span style="background-color: #0d4a8d;">&nbsp;</span></div>` +
-              // `<div class="XbIp4 jmmB7 GNqVo yxtKT allowTextSelection" tabindex="-1" role="region" aria-label="Corpo da mensagem">` +
-              // `<div class="rps_68f">` +
-              // `<div style="background-color: #0d4a8d;"><center>` +
-              // `<div class="R1UVb" style="height: 638px; width: 100%;">` +
-              // `<table id="x_bodyTable" style="transform: scale(0.85259, 0.85259); transform-origin: left top;" border="0" width="100%" cellspacing="0" cellpadding="0" align="center" bgcolor="#0d4a8d">` +
-              // `<tbody>` +
-              // `<tr>` +
-              // `<td id="x_bodyCell" style="padding-bottom: 60px;" align="center" valign="top"><span style="color: #ffe01b; display: none; font-size: 0px; height: 0px; visibility: hidden; width: 0px;">You're almost ready to get started!</span>` +
-              // `<table border="0" width="100%" cellspacing="0" cellpadding="0" align="center">` +
-              // `<tbody>` +
-              // `<tr>` +
-              // `<td style="background-color: #0d4a8d;" align="center" valign="top" bgcolor="#FFE01B">` +
-              // `<table class="x_emailContainer" style="max-width: 640px;" border="0" width="100%" cellspacing="0" cellpadding="0" align="center">` +
-              // `<tbody>` +
-              // `<tr>` +
-              // `<td style="padding: 40px;" align="center" valign="top"><a style="text-decoration: none;" href="https://www.mailchimp.com/" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable" data-linkindex="0"><img style="border: 0; color: #ffffff; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 12px; font-weight: 400; height: auto; letter-spacing: -1px; padding: 0; outline: none; text-align: center; text-decoration: none; border-width: 0px; margin: 0px;" src="https://files.readme.io/694acc9-small-d15893cb-08b5-465e-8c5a-2d783f945adc.jpg" alt="Mailchimp" width="120" data-imagetype="External" /></a></td>` +
-              // `</tr>` +
-              // `<tr>` +
-              // `<td style="background-color: #ffffff; padding-top: 40px;">&nbsp;</td>` +
-              // `</tr>` +
-              // `</tbody>` +
-              // `</table>` +
-              // `</td>` +
-              // `</tr>` +
-              // `<tr>` +
-              // `<td align="center" valign="top">` +
-              // `<table class="x_emailContainer" style="background-color: #ffffff; max-width: 640px;" border="0" width="100%" cellspacing="0" cellpadding="0" align="center" bgcolor="#FFFFFF">` +
-              // `<tbody>` +
-              // `<tr>` +
-              // `<td style="padding-right: 40px; padding-bottom: 40px; padding-left: 40px;" align="center" valign="top" bgcolor="#FFFFFF">` +
-              // `<h1 style="color: #241c15; font-family: Georgia,Times,'Times New Roman',serif; font-size: 30px; font-style: normal; font-weight: 400; line-height: 42px; letter-spacing: normal; margin: 0; padding: 0; text-align: center;">Ol&aacute;, ${nome}!<br aria-hidden="true" />Suas credenciais foram geradas</h1>` +
-              // `</td>` +
-              // `</tr>` +
-              // `<tr>` +
-              // `<td style="padding-right: 40px; padding-bottom: 60px; padding-left: 40px; text-align: left;" align="center" valign="middle">` +
-              // `<p><strong><span style="background-color: #0d4a8d; color: #ffffff;">clientId:</span>&nbsp;</strong> &nbsp; &nbsp; &nbsp; &nbsp;${JSON.parse(response).clientId}</p>` +
-              // `<p><strong><span style="background-color: #0d4a8d; color: #ffffff;">clientSecret:</span></strong>&nbsp; &nbsp;${JSON.parse(response).clientSecret}</p>` +
-              // `</td>` +
-              // `</tr>` +
-              // `<tr>` +
-              // `<td style="padding-right: 40px; padding-bottom: 40px; padding-left: 40px;" align="center" valign="top">` +
-              // `<p style="color: #6a655f; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 16px; font-style: normal; font-weight: 400; line-height: 42px; letter-spacing: normal; margin: 0; padding: 0; text-align: center;">(Aguarde o prazo de at&eacute; 24 horas para ativa&ccedil;&atilde;o do seu usu&aacute;rio.)</p>` +
-              // `</td>` +
-              // `</tr>` +
-              // `<tr>` +
-              // `<td class="x_footerContent" style="border-top: 2px solid #EFEEEA; color: #6a655f; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 12px; font-weight: 400; line-height: 24px; padding-top: 40px; padding-bottom: 40px; text-align: center;" align="center" valign="top">&nbsp;</td>` +
-              // `</tr>` +
-              // `</tbody>` +
-              // `</table>` +
-              // `</td>` +
-              // `</tr>` +
-              // `</tbody>` +
-              // `</table>` +
-              // `</td>` +
-              // `</tr>` +
-              // `</tbody>` +
-              // `</table>` +
-              // `</div>` +
-              // `</center></div>` +
-              // `</div>` +
-              // `</div>` ;
 
               const htmlUsuario = 
               '<div style="margin: 0; padding: 0; font-family: Verdana, Geneva, Tahoma, sans-serif;"> ' +
@@ -166,7 +99,7 @@ router.post('/new', async (req, res, next) => {
               '    <h4>clientId:</h4> ' +
               '</div> ' +
               '<div style="background-color: #474747; color: white; text-align: left;  word-wrap: break-word; padding: 5px;  font-size: 11px; font-family: Arial, sans-serif; font-weight: normal;"> ' +
-              '    <h4>agB1AG4AaQBvAHIAbwBsAGkAdgBlAGkAcgBhAC4AbABhAEAAaABvAHQAbQBhAGkAbAAuAGMAbwBtAA==</h4> ' +
+              '    <h4>' + response.clientId + '</h4> ' +
               '</div> ' +
               '<div style="padding: 5px 10px;  ' +
               '            height: 30px;  ' +
@@ -178,7 +111,7 @@ router.post('/new', async (req, res, next) => {
               '    <h4>clientSecret:</h4> ' +
               '</div> ' +
               '<div style="background-color: #474747; color: white; text-align: left;  word-wrap: break-word; padding: 5px;  font-size: 11px; font-family: Arial, sans-serif; font-weight: normal;"> ' +
-              '    <h4>agB1AG4AaQBvAHIAbwBsAGkAdgBlAGkAcgBhAC4AbABhAEAAaABvAHQAbQBhAGkAbAAuAGMAbwBtAA==</h4> ' +
+              '    <h4>' + response.clientSecret + '</h4> ' +
               '</div> ' +
               '</div>'  +
               '         ' +
